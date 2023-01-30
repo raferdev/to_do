9e60a44fb0ba6c69034bc62e3a567d1b9d8d1947
Rafael <rafinha_fern@hotmail.com>
2023-01-30 01:50:52 -0300

    feat: complete taskInput component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./Global.css";
 import styles from "./App.module.css";
 import { Header } from "./components/Header.js";
+import { TaskInput } from "./components/TaskInput.js";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <TaskInput/>
       <div className={styles.body}></div>
     </div>
   );
--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,7 +1,10 @@
-import styles from "./TaskInput.module.css"
-
+import styles from "./TaskInput.module.css";
+import plusSvgIcon from "../assets/plus.svg"
 export function TaskInput() {
-    return (
-        <input className={styles.input}/>
-    )
+  return (
+    <div className={styles.inputWrap}>
+      <input className={styles.input} placeholder="Create a new task here!" />
+      <button className={styles.createButton}>Create <img src={plusSvgIcon}/></button>
+    </div>
+  );
 }
